e9feaedd674885d8e656d0bce24bf6a3a382a903
Julia Lange <julia.warnke@visuellverstehen.de>
2023-05-04 09:32:02 +0200

    fix: migrations

--- database/migrations/2023_05_04_093107_update_pixxio_files_table.php
+++ database/migrations/2023_05_04_093107_update_pixxio_files_table.php
@@ -4,14 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::table('pixxio_files', function (Blueprint $table) {
-            $table->text('copyright')->nullable();
+            $table->text('alternative_text')->nullable()->change();
         });
     }
 
@@ -20,8 +21,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::table('pixxio_files', function (Blueprint $table) {
-           $table->dropColumn(['copyright']);
-        });
+        //
     }
 };
