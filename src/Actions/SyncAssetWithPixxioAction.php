bb0b1801eeb61eb1460a61b26bf223fd64949d6c
Julia Lange <julia.warnke@visuellverstehen.de>
2023-06-06 15:44:43 +0200

    feat: support syncing multiple assets

--- src/Actions/SyncAssetWithPixxioAction.php
+++ src/Actions/SyncAssetWithPixxioAction.php
@@ -15,33 +15,57 @@ class SyncAssetWithPixxioAction extends Action
 {
     public function run($items, $values)
     {
-        $asset = $items->first();
+        $failed = collect();
+        $succeeded = collect();
 
-        $path = Str::start($asset->path, '/');
+        $items->each(function ($asset) use ($failed, $succeeded) {
+            $path = Str::start($asset->path, '/');
+            $file = PixxioFile::find($path);
 
-        $file = PixxioFile::find($path);
+            try {
+                $client = new Client();
+                $incomingData = $client->getFile($path);
 
-        try {
-            $client = new Client();
-            $incomingData = $client->getFile($path);
+                $file->update([
+                    'absolute_path' => $incomingData['imagePath'],
+                    'filesize' => $incomingData['fileSize'],
+                    'width' => $incomingData['imageWidth'],
+                    'height' => $incomingData['imageHeight'],
+                    'last_modified' => $incomingData['uploadDate'] ?? now()->format('Y-m-d H:i:s'),
+                    'alternative_text' => $incomingData['dynamicMetadata']['Alternativetext'],
+                    'copyright' => $incomingData['dynamicMetadata']['CopyrightNotice'],
+                    'updated_at' => now(),
+                ]);
 
-            $file->update([
-                'absolute_path' => $incomingData['imagePath'],
-                'filesize' => $incomingData['fileSize'],
-                'width' => $incomingData['imageWidth'],
-                'height' => $incomingData['imageHeight'],
-                'last_modified' => $incomingData['uploadDate'] ?? now()->format('Y-m-d H:i:s'),
-                'alternative_text' => $incomingData['dynamicMetadata']['Alternativetext'],
-                'copyright' => $incomingData['dynamicMetadata']['CopyrightNotice'],
-                'updated_at' => now(),
-            ]);
+                Cache::forget($asset->metaCacheKey());
 
-            Cache::forget($asset->metaCacheKey());
-        } catch (\Exception $exception) {
-            ray($exception);
+                $succeeded->push($file);
+            } catch (\Exception $exception) {
+                $failed->push($file);
+            }
+
+        });
+
+        // todo: refactor
+        if ($failed->isNotEmpty()) {
+            if ($failed->count() < 2) {
+                // todo: translate
+                throw new \Exception("{$failed->first()->filename} failed to syncronize.");
+            }
+
+            // todo: translate
+            throw new \Exception("{$failed->count()} files failed to syncronize.");
+        } else {
+
+            if ($succeeded->count() < 2) {
+
+                // todo: translate
+                return "{$succeeded->first()->filename} has been syncronized.";
+            }
+
+            // todo: translate
+            return $succeeded->count() . ' files were syncronized.';
         }
-
-        return __('Asset has been synced with Pixx.io');
     }
 
     public static function title()
