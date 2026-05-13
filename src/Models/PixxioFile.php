89e8d5c709b551f400a1f42e39c559a6f972cd74
vvjlan <julia.lange@visuellverstehen.de>
2026-05-13 16:36:36 +0200

    fix: missing carbon reference

--- src/Models/PixxioFile.php
+++ src/Models/PixxioFile.php
@@ -2,6 +2,7 @@
 
 namespace VV\PixxioFlysystem\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
